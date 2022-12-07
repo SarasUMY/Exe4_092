f7ec5a17ce6bb491138d1ecd0ca81fc6ab4dd555
TA1092 <bagas.saras.ft21@mail.umy.ac.id>
2022-12-07 15:43:01 +0700

    Membuat method Stack untuk memanggil class Stack

--- Exe4_092/Program.cs
+++ Exe4_092/Program.cs
@@ -11,5 +11,12 @@ namespace Exe4_092
         public int[] element;
         public int top;
         public int max;
+
+        public Stack(int size)
+        {
+            element = new int[size];
+            top = -1;
+            max = size;
+        }
     }
 }
